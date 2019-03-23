ed44e2f2b2a4a6b05b66ea3cde1df7f841e8d44c
Mork <anon@users.noreply.github.com>
2019-03-23 22:07:08 +0000

    copied over js-xlsx folder from data-pipeline; Improved README.md; added a pandas converter to strip doen the postcode->oa11->lsoa11cd->msoa11cd->ladcd main lookup file; moved files specifically for the trimmer into trimmer/ ;

---
+++ js-xlsx/abridge_pcds-oa11-lsoa11cd-msoa11cd-ladcd_file.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+OAs_lookup = pd.read_csv ('./data/Postcode_to_Output_Area_to_Lower_Layer_Super_Output_Area_to_Middle_Layer_Super_Output_Area_to_Local_Authority_District_February_2018_Lookup_in_the_UK.csv', dtype='unicode' )
+
+abridged=OAs_lookup.drop(OAs_lookup.columns[2],axis=1)
+abridged=abridged.drop(abridged.columns[10:14],axis=1)
+abridged=abridged.drop(abridged.columns[5],axis=1)
+
+valid=abridged[pd.to_numeric(abridged['doterm'])==0]
+valid=valid.drop(valid.columns[2],axis=1)
+
+recent=abridged[pd.to_numeric(abridged['doterm'])>201000]
+
+recent.to_csv ('./data/pcds-oa11-lsoa11cd-msoa11cd-ladcd_Recently_terminated_postcodes_only.csv', index=False)
+valid.to_csv ('./data/pcds-oa11-lsoa11cd-msoa11cd-ladcd_Valid_postcodes_only.csv', index=False)
+
+
+
