cb4f8c3a6c84c54e91a2d11c59b28a93127497cb
monkeybrie <morkeltry@mailfence.com>
2019-05-21 04:29:08 +0100

    started on bulk-convert.mjs, but unfinished

---
+++ js-xlsx/bulk-convert.mjs
@@ -0,0 +1,86 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import { hierarchies, tellMeAbout, report } from './convert-areas';
+import { indexes, csvRead, loadInventoriedDataset, doIndex } from './ingest-datasets.mjs';
+
+
+
+const printHelpMessage = ()=> {
+  console.log('No arguments given');
+}
+
+const mapCsvRowToMemoryStructure = ((x,lookupFn) => {
+  x = trimUnwantedColumns(x);
+
+});
+
+const mapArgToMemoryStructure = ((x) => x);
+
+
+
+////this function UNFINISHED - possibly on the wrong track!!!
+// do it better - are you confusing parent->child and child->parent maps?
+
+const addToSecondaryIndex = (row, arrIndex) => {
+  checkIsChild ();
+  const newDatasetKey = row[arrIndex];
+  // const {children} = indexes.lsoa11cd[row[arrIndex]] //???
+  const {children} = report(indexes.lsoa11cd[row[arrIndex]]) //???
+  Object.keys(children).forEach (childName => {
+    if (!secondaryIndex[childName])
+      secondaryIndex[childName]={}
+    else
+      secondaryIndex[childName][] += children[childName];
+  })
+
+
+const loadAndIndexInMemory = (filename, args) => {
+  const getPostcodeLookup = loadInventoriedDataset( inventory.postcodes_valid_to_OAs, {maxRows : VALID_A_POSTCODES_PLUS_BIRMINGHAM_B} )
+    .then ( ingestedData => {
+      const convertFromArea = 'OA11';
+      const convertToArea = 'lsoa11cd';
+      const arrIndex = ingestedData.lookup(convertFromArea);
+      doIndex (ingestedData);
+      ingestedData.results.forEach (row=> addToSecondaryIndex (row, arrIndex));
+
+
+
+    });
+  getBatchData = (filename) ?
+    new Promise ((resolve, reject) => {
+      const process = mapCsvRowToMemoryStructure;
+      csvRead (filename, process)
+        .then ( result=> {
+          resolve (result);
+        })
+    })
+    : Promise.resolve (args.map (x=>x) );
+
+  return Promise.all ([getPostcodeLookup, getBatchData])
+}
+
+
+const loadAndIndexInMemory = filename => {
+
+};
+
+
+const args = process.argv.slice (2);
+if (!args.length)
+  printHelpMessage()
+else {
+  const batchFilename = fs.existsSync(args[0]) && args[0]);
+  if (batchFilename)
+    console.log('Thats a file that exists.');
+
+  const loadAndIndex = (batchFilename && (fs.statSync(filename).size > permittedMemory)) ?
+    loadAndIndexAsStream
+    : loadAndIndexInMemory ;
+
+  loadAndIndex (batchFilename || undefined)
+    .then (stuff => {
+      // doStuff()
+
+
+    });
+}
--- js-xlsx/convert-areas.mjs
+++ js-xlsx/convert-areas.mjs
@@ -3,12 +3,15 @@ import anyName from './GSS-decoders';
 import inventory from './inventory';
 import { indexes, csvRead, loadInventoriedDataset, doIndex } from './ingest-datasets.mjs';
 
+const thisModule = 'convert-areas.';
+const runDirectFromCli = process.argv[1].split('/').pop().startsWith(thisModule);
+
 // pass these as maxRows to limit the amount of data loaded!
 const VALID_ABERDEEN_POSTCODES = 18171;
 const VALID_A_POSTCODES_PLUS_BIRMINGHAM_B = 62042;
 
 // TODO: DOUBLE CHECK - is MSOA11CD actually a child of LAD17CD???
-const heirarchies = [
+const hierarchies = [
   ['PCDS', 'OA11', 'LSOA11CD', 'MSOA11CD', 'LAD17CD']
 ]
 
@@ -17,10 +20,10 @@ const isChildOf = (childCodeType, parentCodeType) => {
 console.log('compare:',childCodeType, parentCodeType );
   childCodeType = childCodeType.toUpperCase();
   parentCodeType = parentCodeType.toUpperCase();
-  return (heirarchies
-    .filter (heirarchy => heirarchy.includes(parentCodeType))
-    .some (heirarchy => {
-      const childCandidate = heirarchy[ heirarchy.indexOf(parentCodeType) -1 ] ;
+  return (hierarchies
+    .filter (hierarchy => hierarchy.includes(parentCodeType))
+    .some (hierarchy => {
+      const childCandidate = hierarchy[ hierarchy.indexOf(parentCodeType) -1 ] ;
       if (childCandidate==='PCDS' && childCodeType.startsWith('POSTCODE'))
         return true
       return (childCodeType===childCandidate || childCodeType===childCandidate+'S');
@@ -39,7 +42,7 @@ const relations = codeType => {
   }
   const wholeParents = [], wholeChildren = [], partialParents = [], partialChildren = [];
 
-  // TODO: Generalise for any heirarchy
+  // TODO: Generalise for any hierarchy
   if (codeType==='pcds')
     wholeParents.push ('OA11');
   if (codeType==='OA11' || wholeParents.includes('OA11'))
@@ -157,7 +160,7 @@ const report = ( gssCode, options={} ) => {
     Object.assign (response, relations(response.codeType||response.codeTypeBeingUsedForNow));
   }
   else {
-    // unknown code type - don't bother with heirarchy
+    // unknown code type - don't bother with hierarchy
   }
 
   response.indexed = !(indexedAs===undefined);
@@ -253,33 +256,46 @@ console.log('possibles:' , Object.keys(indexes[indexedAs][gssCode]) );
 // console.log(isChildOf('OA11S', 'LSOA11CD'));
 // console.log(isChildOf('OA11', 'LSOA11CDS'));
 
-loadInventoriedDataset( inventory.postcodes_valid_to_OAs, {maxRows : VALID_A_POSTCODES_PLUS_BIRMINGHAM_B} )
-  .then ( results => {
-    doIndex (results)
+// if called as a module rather than run from CLI, do nothing - leave it to the parent module.
+if (runDirectFromCli) {
+  loadInventoriedDataset( inventory.postcodes_valid_to_OAs, {maxRows : VALID_A_POSTCODES_PLUS_BIRMINGHAM_B} )
+    .then ( results => {
+      doIndex (results);
 
-    // real OA in AB10 ( particularly large OA in centre of town at the cross of multiple postcodes)
-    tellMeAbout('S00090540');
+      const args = process.argv.slice (2);
+      if (args.length) {
+        console.log(args);
+        args.forEach (tellMeAbout);
+      } else {
+        // real OA in AB10 ( particularly large OA in centre of town at the cross of multiple postcodes)
+        tellMeAbout('S00090540');
 
-    // fake English ward
-    tellMeAbout('E05090540');
+        // fake English ward
+        tellMeAbout('E05090540');
 
-    // real OA  in B99
-    tellMeAbout('E00045170');
+        // real OA  in B99
+        tellMeAbout('E00045170');
 
-    // real OA  in B62
-    tellMeAbout('E00049607');
+        // real OA  in B62
+        tellMeAbout('E00049607');
 
-    // real LSOA, Bromsgrove
-    tellMeAbout('E01032176');
+        // real LSOA, Bromsgrove
+        tellMeAbout('E01032176');
 
-    console.log('\nS00090540');
-    console.log(report('S00090540'));
-    console.log('\nE05090540');
-    console.log(report('E05090540'));
-    // console.log('\nE00045170');
-    // console.log(report('E00045170'));    // loadsa output!
-    console.log('\nE00049607');
-    console.log(report('E00049607'));
-    // console.log('\nE01032176');
-    // console.log(report('E01032176'));
-  });
+        console.log('\nS00090540');
+        console.log(report('S00090540'));
+        console.log('\nE05090540');
+        console.log(report('E05090540'));
+        // console.log('\nE00045170');
+        // console.log(report('E00045170'));    // loadsa output!
+        console.log('\nE00049607');
+        console.log(report('E00049607'));
+        // console.log('\nE01032176');
+        // console.log(report('E01032176'));
+      };
+    });
+  };
+
+
+
+  export { hierarchies, tellMeAbout, report }
